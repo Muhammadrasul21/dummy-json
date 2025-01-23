2e925042806e6001d162642b9d6abe1e00f733da
Muhammadrasul21 <theboy6878@gmail.com>
2025-01-24 01:42:02 +0500

    added 404

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Contact from "./pages/contact/Contact"
 import Login from "./pages/Login/Login"
 import Delivery from "./pages/delivery/Delivery"
 import Detail from './pages/detail/Detail'
+import NotFound from './pages/not-found/NotFound'
 
 function App() {
   const [show, setShow] = React.useState(false)
@@ -20,7 +21,7 @@ function App() {
       <Route path="/login" element={<Login/>}/>
       <Route path="/delivery" element={<Delivery/>}/>
       <Route path="/product/:id" element={<Detail/>}/>
-      <Route path="*" element={<Detail/>}/>
+      <Route path='*' element={<NotFound/>}/>
     </Routes>
     </>
   )
--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div className='m-auto'>NotFound</div>
+    <div className='m-auto'>
+      <div className='flex flex-col items-center mt-20'>
+      <h2 className='text-4xl font-semibold mb-10'>Not Found</h2>
+      <p className='text-9xl font-bold lead tracking-wide'>404</p>
+      </div>
+    </div>  
   )
 }
 
